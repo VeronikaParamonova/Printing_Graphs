af9db0d1a468d4cb640cad6392fb561587a1833f
VeronikaParamonova <75235582+VeronikaParamonova@users.noreply.github.com>
2026-05-29 13:59:46 +0700

    Добавление IOC контейнера
    Добавление предоставленного IOC контейнера для его тестирования и дальнейшей работы

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include "ioccontainer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    //------Example #1----------------
+    //Injector injector;
+    IOCContainer injector;
+
+    // Регистрируем IHello с классом Hello, т.о. каждый раз запрашивая IHell получаем объект Hello.
+    injector.RegisterInstance<IHello, Hello>();
+    auto helloInstance = injector.GetObject<IHello>();
+    helloInstance->hello();
+    injector.RegisterInstance<IHello, Privet>();
+
+    //Здесь, после регистрации получим объект Privet
+    helloInstance = injector.GetObject<IHello>();
+    helloInstance->hello();
+
+    //------Example #2----------------
+
+    gContainer.RegisterInstance<IAmAThing, TheThing>();
+    gContainer.RegisterFactory<IAmTheOtherThing, TheOtherThing, IAmAThing>();
+
+    gContainer.GetObject<IAmAThing>()->TestThis();
+    gContainer.GetObject<IAmTheOtherThing>()->TheOtherTest();
+
+    //Опять запршиваем объект,после последней регистрации получим объект Privet
+    helloInstance = injector.GetObject<IHello>();
+    helloInstance->hello();
+
+    return a.exec();
+}
